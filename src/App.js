b8ef99b57f9bc6f524ce2afb4f5add11c8e0f453
Quentin Rouchet <quentin.rouchet@outlook.com>
2022-11-23 16:21:48 +0100

    Set up react and folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* Inport */
+import './App.css';
+import Search from "./components/search/search"
+
+/* SASS import */
+import "./sass/pages/home.scss"
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
