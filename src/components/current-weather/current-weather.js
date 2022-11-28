44be697dceb9639c7a81852b2bb776f44f3ea084
Quentin Rouchet <quentin.rouchet@outlook.com>
2022-11-28 16:05:18 +0100

    add design and responsive on weather app

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,32 +4,33 @@ const CurrentWeather = ({data}) => {
     return (
         <div className='weather'>
             <div className='weather--top'>
-                <div>
-                    <p className='city'>{data.city}</p>
-                    <p className='weather-description'>{data.weather[0].description}</p>
-                </div>
                 <img src={require(`../../icon/${data.weather[0].icon}.png`)} alt="weather" className='weather-icon' />
-            </div>
-            <div className='weather--bottom'>
+                <div>
+                    <p className='weather-description'>{data.weather[0].description}</p>
+                    <p className='city'>{data.city}</p>
+                </div>
                 <p className='temperature'>{Math.round(data.main.temp)}°C</p>
+            </div>
+            
+            <div className='weather--bottom'>
                 <div className='details'>
                     <div className='parameter-row'>
-                        <span className='parameter-label top'>Details</span>
-                    </div>
-                    <hr/>
-                    <div className='parameter-row'>
+                        <img src={require(`../../icon/thermometer.png`)} alt="icon" className='details__icon' />
                         <span className='parameter-label'>Feels like</span>
                         <span className='parameter-value'>{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className='parameter-row'>
+                        <img src={require(`../../icon/wind.png`)} alt="icon" className='details__icon' />
                         <span className='parameter-label'>Wind</span>
                         <span className='parameter-value'>{data.wind.speed} m/s</span>
                     </div>
                     <div className='parameter-row'>
+                        <img src={require(`../../icon/water-drop.png`)} alt="icon" className='details__icon' />
                         <span className='parameter-label'>Humidity</span>
                         <span className='parameter-value'>{data.main.humidity}%</span>
                     </div>
                     <div className='parameter-row'>
+                        <img src={require(`../../icon/pressure.png`)} alt="icon" className='details__icon' />
                         <span className='parameter-label'>Pressure</span>
                         <span className='parameter-value'>{data.main.pressure} hPa</span>
                     </div>
